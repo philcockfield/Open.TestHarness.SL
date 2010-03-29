f46af372093ccc3ad1aa19adff2300e89485a7cb
Phil Cockfield <phil@cockfield.net>
2010-03-29 10:33:55 -0700

    Added change events to ContentContainer.

--- Source/Open.Core/Silverlight/Open.Core.Contracts/Interfaces/Core.UI.Controls/Containers/IContentContainer.cs
+++ Source/Open.Core/Silverlight/Open.Core.Contracts/Interfaces/Core.UI.Controls/Containers/IContentContainer.cs
@@ -8,6 +8,15 @@ namespace Open.Core.UI.Controls
     /// <summary>A flexible content container that can safely work with bound UIElements or DataTemplates.</summary>
     public interface IContentContainer : IViewFactory, INotifyPropertyChanged, INotifyDisposed
     {
+        /// <summary>Fires when the 'Content' value changes.</summary>
+        event EventHandler ContentChanged;
+
+        /// <summary>Fires when the 'ContentTemplate' value changes.</summary>
+        event EventHandler ContentTemplateChanged;
+
+        /// <summary>Fires when the 'Model' value changes.</summary>
+        event EventHandler ModelChanged;
+
         /// <summary>Gets or sets the data-template used to render the content (this is overridden by 'Content' if set).</summary>
         DataTemplate ContentTemplate { get; set; }
 
