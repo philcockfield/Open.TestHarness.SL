175abb47cf16a6790a615425be99e1f370357c47
Phil Cockfield <phil@cockfield.net>
2010-04-08 20:33:38 -0700

    Created simple control for rendering an icon within the ImageIcon enum set.

---
+++ Source/Open.Core/Silverlight/Open.Core.UI.Controls/Controls/ImageIcon.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Open.Core.Common;
+
+namespace Open.Core.UI.Controls
+{
+    /// <summary>A simple way of rendering an 'IconImage' in XAML.</summary>
+    public partial class ImageIcon : UserControl
+    {
+        #region Head
+        /// <summary>Constructor.</summary>
+        public ImageIcon()
+        {
+            InitializeComponent();
+            OnSourceChanged();
+        }
+        #endregion
+
+        #region Dependency Properties
+        /// <summary>Gets or sets the enum representing the icon image.</summary>
+        public IconImage Source
+        {
+            get { return (IconImage)(GetValue(SourceProperty)); }
+            set { SetValue(SourceProperty, value); }
+        }
+        /// <summary>Gets or sets the enum representing the icon image.</summary>
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register(
+                LinqExtensions.GetPropertyName<ImageIcon>(m => m.Source),
+                typeof(IconImage),
+                typeof(ImageIcon),
+                new PropertyMetadata(IconImage.SilkAccept, (s, e) => ((ImageIcon)s).OnSourceChanged()));
+        private void OnSourceChanged()
+        {
+            iconImage.Source = Source.ToImageSource();
+        }
+        #endregion
+    }
+}
---
+++ Source/Open.Core/Silverlight/Test/Open.Core.Test/ViewTests/Core.Controls/IconImage.ViewTest.cs
@@ -0,0 +1,33 @@
+﻿using Open.Core.Common;
+using Open.Core.UI.Controls;
+
+namespace Open.Core.Test.ViewTests.Core.Controls
+{
+    [ViewTestClass]
+    public class IconImageViewTest
+    {
+        #region Head
+
+        [ViewTest(Default = true, IsVisible = false)]
+        public void Initialize(ImageIcon control)
+        {
+            control.Width = 16;
+            control.Height = 16;
+        }
+        #endregion
+
+        #region Tests
+        [ViewTest]
+        public void Icon_Accept(ImageIcon control)
+        {
+            control.Source = IconImage.SilkAccept;
+        }
+
+        [ViewTest]
+        public void Icon_Bug(ImageIcon control)
+        {
+            control.Source = IconImage.SilkBug;
+        }
+        #endregion
+    }
+}
