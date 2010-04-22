f409a7e6f978035fa42648e3b1b30441ac24ad3d
Phil Cockfield <phil@cockfield.net>
2010-04-22 10:19:00 -0700

    Common parser base class.

---
+++ Source/Open.Core/Clr/Test/Open.Core.Test/Core/Base Classes/ParserBaseTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Core.Common.Testing;
+
+namespace Open.Core.Common.Test.Core.Base_Classes
+{
+    [TestClass]
+    public class ParserBaseTest
+    {
+        #region Head
+        private string sampleCsv;
+
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            var reader = new StreamReader(GetStream());
+            sampleCsv = reader.ReadToEnd();
+        }
+
+        private static Stream GetStream()
+        {
+            return "/Core/Base Classes/Sample Data/ParseSample.csv".LoadEmbeddedResource();
+        }
+        #endregion
+        
+        #region Tests
+        [TestMethod]
+        public void ShouldLoadSampleCsv()
+        {
+            sampleCsv.ShouldNotBe(null);
+        }
+
+        [TestMethod]
+        public void ShouldExposeRawText()
+        {
+            var parser = new Mock(sampleCsv);
+            parser.RawText.ShouldBe(sampleCsv);
+            parser = new Mock("  ");
+            parser.RawText.ShouldBe(null);
+        }
+
+        [TestMethod]
+        public void ShouldConstructFromStream()
+        {
+            var parser = new Mock(GetStream());
+            parser.RawText.ShouldBe(sampleCsv);
+        }
+
+        [TestMethod]
+        public void ShouldThrowWhenNullStreamIsPassedToConstructor()
+        {
+            Should.Throw<ArgumentNullException>(() => new Mock((Stream)null));
+        }
+
+        [TestMethod]
+        public void ShouldGetLinesCollection()
+        {
+            var parser = new Mock(GetStream());
+            parser.Lines.Count().ShouldBe(3);
+
+            parser = new Mock("");
+            parser.Lines.Count().ShouldBe(0);
+        }
+
+
+        [TestMethod]
+        public void ShouldReturnEmptyJobCollectionWhenNoContentToParse()
+        {
+            var parser = new Mock("");
+            parser.Models.Count().ShouldBe(0);
+        }
+
+        [TestMethod]
+        public void ShouldHaveSameNumberOfModelsAsLines()
+        {
+            var parser = new Mock(GetStream());
+            parser.Models.Count().ShouldBe(parser.Lines.Count());
+        }
+
+        [TestMethod]
+        public void ShouldParseModels()
+        {
+            var parser = new Mock(GetStream());
+            parser.Models.ElementAt(0).Name.ShouldBe("Fred");
+            parser.Models.ElementAt(1).Name.ShouldBe(null);
+            parser.Models.ElementAt(2).Name.ShouldBe("Jane");
+
+            parser.Models.ElementAt(0).Value.ShouldBe("One");
+            parser.Models.ElementAt(1).Value.ShouldBe("Two");
+            parser.Models.ElementAt(2).Value.ShouldBe(null);
+        }
+        #endregion
+
+        public class Mock : ParserBase<MyModel>
+        {
+            public Mock(Stream stream) : base(stream){}
+            public Mock(string rawText) : base(rawText){}
+
+            protected override MyModel CreateModel(string[] fields)
+            {
+                return new MyModel
+                           {
+                               Name = fields.ElementAtOrDefault(0).AsNullWhenEmpty(),
+                               Value = fields.ElementAtOrDefault(1).AsNullWhenEmpty(),
+                           };
+            }
+        }
+
+        public class MyModel
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+        }
+
+    }
+}
---
+++ Source/Open.Core/Silverlight/Open.Core/Base Classes/ParserBase.cs
@@ -0,0 +1,110 @@
+﻿//------------------------------------------------------
+//    Copyright (c) 2010 TestHarness.org
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the 'Software'), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in
+//    all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//    THE SOFTWARE.
+//------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Open.Core.Common
+{
+    /// <summary>Provides common base functionality for parsing files.</summary>
+    public abstract class ParserBase<TModel>
+    {
+        #region Head
+        private IEnumerable<TModel> models;
+        private string delimiter;
+        private static readonly string defaultDelimiter = string.Format("\t");
+
+        /// <summary>Constructor.</summary>
+        /// <param name="stream">Stream containing the CSV file.</param>
+        protected ParserBase(Stream stream) : this(ReadStream(stream)) { }
+
+        /// <summary>Constructor.</summary>
+        /// <param name="rawText">The raw CSV text.</param>
+        protected ParserBase(string rawText)
+        {
+            RawText = rawText.AsNullWhenEmpty();
+            Lines = RawText == null 
+                                ? new string[] {}
+                                : RawText.Split(Environment.NewLine.ToCharArray()).Where(m => !m.IsNullOrEmpty(true));
+
+            Delimiter = defaultDelimiter;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>Gets or sets the CSV delimiter character.</summary>
+        public string Delimiter
+        {
+            get { return delimiter; }
+            set
+            {
+                delimiter = value.AsNullWhenEmpty();
+                models = null;
+            }
+        }
+
+        /// <summary>Gets the raw CSV text.</summary>
+        public string RawText { get; private set; }
+
+        /// <summary>Gets the collection of lines that make up the file.</summary>
+        public IEnumerable<string> Lines { get; private set; }
+
+        /// <summary>Gets the collection of models parsed out of the CSV file.</summary>
+        public IEnumerable<TModel> Models { get { return models ?? (models = Parse()); } }
+        #endregion
+
+        #region Methods
+
+        /// <summary>Implemented in the deriving class to convert the fields from the given line into it's corresonding model.</summary>
+        /// <param name="fields">The collection of fields split from a single line within the CSV file.</param>
+        protected abstract TModel CreateModel(string[] fields);
+        #endregion
+
+        #region Internal
+        private static string ReadStream(Stream stream)
+        {
+            if (stream == null) throw new ArgumentNullException("stream");
+            var reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+
+        private IEnumerable<TModel> Parse()
+        {
+            // Setup initial conditions.
+            if (Lines.Count() == 0) return new TModel[] { };
+            var list = new List<TModel>();
+
+            // Enumerate the collection of lines within the CSV file.
+            foreach (var line in Lines)
+            {
+                var fields = line.Split((Delimiter ?? defaultDelimiter).ToCharArray());
+                list.Add(CreateModel(fields));
+            }
+
+            // Finish up.
+            return list;
+        }
+        #endregion
+    }
+}
