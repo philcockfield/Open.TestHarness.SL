cfce04afc3aad56eba8b18964d50b0b7faa5e8e9
philcockfield <phil@cockfield.net>
2010-05-05 22:41:01 -0700

    Renamed IPageQuery to IPageFilter

---
+++ Source/Open.Core/Clr/Test/Open.Core.Test/Core/Model/Filter/PageFilterTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Core.Common.Testing;
+
+namespace Open.Core.Common.Test.Core.Model.Filter
+{
+    [TestClass]
+    public class PageFilterTest
+    {
+        [TestMethod]
+        public void ShouldHaveDefaultValues()
+        {
+            var model = new PageFilter();
+            model.TakeCount.ShouldBe(int.MaxValue);
+            model.SkipCount.ShouldBe(0);
+        }
+    }
+}
