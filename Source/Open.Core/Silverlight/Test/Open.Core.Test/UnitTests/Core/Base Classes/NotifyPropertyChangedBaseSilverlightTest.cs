9896adf5522805c68e6408a36b36a6987bfdf3cc
philcockfield <phil@cockfield.net>
2010-06-09 17:56:23 -0700

    Put the event bus on the NotifyPropertyChangedBase

---
+++ Source/Open.Core/Silverlight/Test/Open.Core.Test/UnitTests/Core/Base Classes/NotifyPropertyChangedBaseSilverlightTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Core.Common;
+using Open.Core.Common.Testing;
+using Open.Core.Composite;
+
+namespace Open.Core.Test.UnitTests.Core.Model
+{
+    [TestClass]
+    public class NotifyPropertyChangedBaseSilverlightTest : SilverlightUnitTest
+    {
+        #region Head
+        private Mock mock;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            mock = new Mock();
+        }
+        #endregion
+        
+        #region Tests
+        [TestMethod]
+        public void ShouldHaveSingletonEventBus()
+        {
+            mock.EventBus.ShouldNotBe(null);
+            mock.EventBus.ShouldBe(EventBus);
+        }
+        #endregion
+
+        public class Mock : NotifyPropertyChangedBase
+        {
+            public new IEventBus EventBus { get { return base.EventBus; } }
+        }
+
+    }
+}
