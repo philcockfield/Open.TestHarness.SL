dd890c595aa6d64e83192cd6a8279c6ab585de67
philcockfield <phil@cockfield.net>
2010-10-25 16:15:42 -0700

    Upgrade to Script# - 0.6.2

--- Source/Open.Core/JavaScript/Open.Library.Jit/Constants/Elements.cs
+++
@@ -1,7 +0,0 @@
-namespace Open.Library.Jit
-{
-    /// <summary>Constants for element IDs.</summary>
-    public static class Elements
-    {
-    }
-}
--- Source/Open.Core/JavaScript/Open.Library.Jit/Constants/JitCss.cs
+++
@@ -1,8 +0,0 @@
-namespace Open.Library.Jit
-{
-    /// <summary>Constants for common CSS selectors.</summary>
-    public static class JitCss
-    {
-        public const string HypertreeUrl = "/Open.Core/Css/Jit.Hypertree.css";
-    }
-}
